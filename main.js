1517eacd938d8822c68207b25d0e70758dcf9e4e
ガビ <104731831+GabyGtzP@users.noreply.github.com>
2022-12-14 22:05:50 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+let app = document.getElementById('typewriter');
+ 
+let typewriter = new Typewriter(app, {
+  loop: true,
+  delay: 75,
+});
+ 
+typewriter
+  .pauseFor(2500)
+  .typeString('Data Scientist and Full-Stack developer.')
+  .pauseFor(200)
+  .deleteChars(10)
+  .start();
+
